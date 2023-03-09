0f7931d3b208cbd44d0fbb12d1a9a6be06d9abd7
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-03-09 00:38:01 +0000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Declan Gallagher on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
